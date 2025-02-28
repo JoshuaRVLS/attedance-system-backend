52ac630c62b80f4a2ef425b5576a9971001d38f4
Joshua  Ravael <jravaellnew@gmail.com>
2025-02-28 13:47:48 +0700

    Add new logging system

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,41 @@
 import express from "express";
-import { sequelize } from "./config/database"; 
-import cors from 'cors';
+import { sequelize } from "./config/database";
+import cors from "cors";
 import StudentRouter from "./routers/student.router";
-import ClassRouter from './routers/class.router';
+import ClassRouter from "./routers/class.router";
 import { urlencoded } from "body-parser";
-import http from 'http';
+import http from "http";
 import { Server } from "socket.io";
 import { socketHandler } from "./socket/handler";
+import BeautifyConsole from "beautify-console-log";
 
 const app = express();
-app.use(cors({
-    allowedHeaders: ['*'],
-    origin: ['http://localhost:3000'],
+app.use(
+  cors({
+    allowedHeaders: ["*"],
+    origin: ["http://localhost:3000"],
     credentials: true,
-    methods: ['*'],
-}));
+    methods: ["*"],
+  })
+);
 app.use(express.json());
-app.use(urlencoded({extended: true}))
-app.use('/api/v1/students', StudentRouter);
-app.use('/api/v1/classes', ClassRouter)
+app.use(urlencoded({ extended: true }));
+app.use("/api/v1/students", StudentRouter);
+app.use("/api/v1/classes", ClassRouter);
 
 const server = http.createServer(app);
 export const io = new Server(server, {
-    cors: {
-        origin: ['http://localhost:3000']
-    }
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
 });
+export const log = BeautifyConsole.getInstance();
+log.config({ title: "BACKEND" });
 
 (async () => {
-    await sequelize.sync({});
-    socketHandler(io);
-    server.listen(8000, '0.0.0.0', () => console.log("listening on port 8000"));
+  await sequelize.sync({});
+  socketHandler(io);
+  server.listen(8000, "0.0.0.0", () =>
+    log.info("Backend Online in 0.0.0.0:8000")
+  );
 })();
