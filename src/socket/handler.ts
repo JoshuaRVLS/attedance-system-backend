ad7865eb0db98d83ba2b0542cd543f8d5004e417
Joshua  Ravael <jravaellnew@gmail.com>
2025-02-28 13:48:10 +0700

    Added readable log

--- src/socket/handler.ts
+++ src/socket/handler.ts
@@ -1,11 +1,11 @@
 import { Server } from "socket.io";
+import { log } from "..";
 
 export const socketHandler = (io: Server) => {
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
+  io.on("connection", (socket) => {
+    log.log("Web Socket connected from ", socket.id);
+    socket.on("disconnect", () => {
+      log.log("Web Socket disconnected from ", socket.id);
     });
-}
-
+  });
+};
