62b39988931605e5361e8d16ec0089fb13a6e547
Joshua Ganteng <jravaellnew@gmail.com>
2025-05-04 20:00:54 +0700

    Use default express app to listen on port 8000

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,6 @@ import cors from "cors";
 import StudentRouter from "./routers/student.router";
 import ClassRouter from "./routers/class.router";
 import { urlencoded } from "body-parser";
-import http from "http";
-import { Server } from "socket.io";
-import { socketHandler } from "./socket/handler";
 import BeautifyConsole from "beautify-console-log";
 
 const app = express();
@@ -23,19 +20,10 @@ app.use(urlencoded({ extended: true }));
 app.use("/api/v1/students", StudentRouter);
 app.use("/api/v1/classes", ClassRouter);
 
-const server = http.createServer(app);
-export const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000"],
-  },
-});
 export const log = BeautifyConsole.getInstance();
 log.config({ title: "BACKEND" });
 
 (async () => {
   await sequelize.sync({});
-  socketHandler(io);
-  server.listen(8000, "0.0.0.0", () =>
-    log.info("Backend Online in 0.0.0.0:8000")
-  );
+  app.listen(8000, "0.0.0.0", () => log.info("Backend Online in 0.0.0.0:8000"));
 })();
